58851aa30d7ac6495c7bceab1f6289e008d47f66
yuchan <lumir1031@gmail.com>
2025-08-13 21:55:49 +0800

    feat: enhance SleepRecord wake_at validation - must be after sleep_at - cannot be modified once it has been set - cannot be blank

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -2,18 +2,30 @@ class SleepRecord < ApplicationRecord
   belongs_to :user
 
   validates :sleep_at, presence: true
-  validate :wake_at_cannot_be_cleared, if: :wake_at_changed?
+  validate :validate_wake_at, if: :wake_at_changed?
   before_save :calculate_duration
 
   scope :from_following, ->(follower_id) {
     where("user_id IN (SELECT followed_id FROM follows WHERE follower_id = ?)", follower_id)
   }
   scope :in_a_week, -> { where(created_at: 7.days.ago..Time.current) }
-  
+
   private
 
-  def wake_at_cannot_be_cleared
-    errors.add(:wake_at, :blank) if wake_at_was.present? && wake_at.blank?
+  def validate_wake_at
+    if wake_at_was.present? && wake_at != wake_at_was
+      errors.add(:wake_at, "cannot be updated once set")
+      return
+    end
+
+    if wake_at.blank?
+      errors.add(:wake_at, "can't be blank")
+      return
+    end
+
+    if wake_at.present? && sleep_at.present? && wake_at <= sleep_at
+      errors.add(:wake_at, "must be after sleep time")
+    end
   end
 
   def calculate_duration
--- spec/models/sleep_record_spec.rb
+++ spec/models/sleep_record_spec.rb
@@ -10,14 +10,65 @@ RSpec.describe SleepRecord, type: :model do
       expect(sleep_record.errors[:sleep_at]).to include("can't be blank")
     end
 
-    context 'when wake_at is being cleared' do
+    context 'when wake_at is already set' do
       let(:sleep_record) { create(:sleep_record, user: user, wake_at: '2025-01-16 07:30:00') }
+
+      it 'prevents updating wake_at once it has been set' do
+        sleep_record.wake_at = '2025-01-16 08:00:00'
+
+        expect(sleep_record).not_to be_valid
+        expect(sleep_record.errors[:wake_at]).to include("cannot be updated once set")
+      end
 
       it 'prevents clearing wake_at once it has been set' do
         sleep_record.wake_at = nil
 
         expect(sleep_record).not_to be_valid
-        expect(sleep_record.errors[:wake_at]).to include("can't be blank")
+        expect(sleep_record.errors[:wake_at]).to include("cannot be updated once set")
+      end
+
+      it 'allows saving without changing wake_at' do
+        expect(sleep_record.save).to be true
+      end
+    end
+
+    context 'when updating wake_at' do
+      it 'allows creating record without setting wake_at initially' do
+        sleep_record = build(:sleep_record, user: user)
+
+        expect(sleep_record).to be_valid
+        expect(sleep_record.wake_at).to be_nil
+      end
+
+      it 'allows first-time setting of valid wake_at' do
+        sleep_record = create(:sleep_record, user: user, sleep_at: '2025-01-16 00:30:00')
+        sleep_record.wake_at = '2025-01-16 08:30:00'
+
+        expect(sleep_record).to be_valid
+      end
+    end
+
+    context 'when wake_at is not after sleep_at' do
+      let(:sleep_at) { Time.parse('2025-01-16 00:30:00') }
+
+      it 'prevents wake_at from being equal to sleep_at' do
+        sleep_record = build(:sleep_record, user: user, sleep_at: sleep_at, wake_at: sleep_at)
+
+        expect(sleep_record).not_to be_valid
+        expect(sleep_record.errors[:wake_at]).to include("must be after sleep time")
+      end
+
+      it 'prevents wake_at from being before sleep_at' do
+        sleep_record = build(:sleep_record, user: user, sleep_at: sleep_at, wake_at: sleep_at - 1.hour)
+
+        expect(sleep_record).not_to be_valid
+        expect(sleep_record.errors[:wake_at]).to include("must be after sleep time")
+      end
+
+      it 'allows wake_at to be after sleep_at' do
+        sleep_record = build(:sleep_record, user: user, sleep_at: sleep_at, wake_at: sleep_at + 8.hours)
+
+        expect(sleep_record).to be_valid
       end
     end
   end
@@ -36,18 +87,6 @@ RSpec.describe SleepRecord, type: :model do
         sleep_record.update!(wake_at: '2025-01-16 06:30:00')
 
         expect(sleep_record.duration).to eq(25200) # 7 hours = 25200 seconds
-      end
-    end
-
-    context 'when wake_at is updated multiple times' do
-      it 'recalculates duration each time' do
-        # First update
-        sleep_record.update!(wake_at: '2025-01-16 06:30:00')
-        expect(sleep_record.duration).to eq(25200) # 7 hours = 25200 seconds
-
-        # Second update
-        sleep_record.update!(wake_at: '2025-01-16 08:30:00')
-        expect(sleep_record.duration).to eq(32400) # 9 hours = 32400 seconds
       end
     end
   end
@@ -70,7 +109,7 @@ RSpec.describe SleepRecord, type: :model do
 
       it 'returns records from followed users only' do
         records = SleepRecord.from_following(follower.id)
-        
+
         expect(records).to include(followed_record1, followed_record2)
         expect(records).not_to include(unfollowed_record)
       end
@@ -78,7 +117,7 @@ RSpec.describe SleepRecord, type: :model do
       it 'returns empty result when user follows nobody' do
         lonely_user = create(:user)
         records = SleepRecord.from_following(lonely_user.id)
-        
+
         expect(records).to be_empty
       end
     end
